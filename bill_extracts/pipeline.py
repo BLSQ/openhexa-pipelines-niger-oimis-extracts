ecb9d0574bae2b13b0e7c57faa07fb727da7100f
EMontandon <esteban14m@gmail.com>
2024-06-20 17:10:25 +0200

    Date range filter implementations

--- bill_extracts/pipeline.py
+++ bill_extracts/pipeline.py
@@ -1,32 +1,57 @@
 import os
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+# from dateutil.relativedelta import relativedelta
 import papermill as pm
+import calendar
 
 from openhexa.sdk import current_run, pipeline, parameter, workspace
 
 
 @pipeline(code="bill-extracts", name="bill_extracts")
 @parameter(
-    "date_year",
-    name="Extract year (e.g: 2024)",
-    help="Execute pipeline for specified year",
+    "date_year_from",
+    name="Extract year from (e.g: 2024)",
+    help="Execute pipeline from specified year",
     type=str,
-    default="2024",
+    default=None,
     required=False,
 )
 @parameter(
-    "date_month",
-    name="Extract month (e.g: 03)",
-    help="Execute pipeline for specified month",
+    "date_month_from",
+    name="Extract month from (e.g: 01)",
+    help="Execute pipeline from specified month",
     type=str,
-    default="03",
+    default=None,
     required=False,
 )
-def bill_extracts(date_year:str, date_month:str):
+@parameter(
+    "date_year_to",
+    name="Extract year to (e.g: 2024)",
+    help="Execute pipeline until specified year",
+    type=str,
+    default=None,
+    required=False,
+)
+@parameter(
+    "date_month_to",
+    name="Extract month to (e.g: 03)",
+    help="Execute pipeline untlil specified month",
+    type=str,
+    default=None,
+    required=False,
+)
+@parameter(
+    "run_auto",
+    name="Automatic run (previous month)",
+    help="Execute pipeline for previous month",
+    type=bool,
+    default=True,
+    required=False,
+)
+def bill_extracts(run_auto:bool, date_year_from:str, date_month_from:str, date_year_to:str, date_month_to:str):
     """
-    In this pipeline we call a notebook launcher that executes the DSE tables update for COUSP local DB
+    In this pipeline we call a notebook that executes the graph queries
     
     """
 
@@ -35,19 +60,42 @@ def bill_extracts(date_year:str, date_month:str):
     notebook_path = f"{workspace.files_path}/pipelines/Bill_extracts/" 
     out_notebook_path = f"{workspace.files_path}/pipelines/Bill_extracts/papermill_outputs"
     
-    # previous_month_date = current_date - relativedelta(months=1)
-    if not valid_year_check(date_year):            
-        return 
-    
-    if not valid_month_check(date_month):
-        return 
-    
-    date_month = int(date_month)
+
+    if run_auto:
+        dateBill_Gte, dateBill_Lte = get_previous_month_dates()
+        current_run.log_info(f"Automatic run from: {dateBill_Gte} to: {dateBill_Lte}")    
+
+    else:
+        # Date input checks - FROM
+        if date_year_from is None and date_month_from is None:
+            dateBill_Gte = None # then we dont filter .. download everything
+        else:
+            if not valid_year_check(date_year_from):            
+                return 
+            if not valid_month_check(date_month_from):
+                return             
+            date_month_from = int(date_month_from)
+            dateBill_Gte = f"{date_year_from}-{date_month_from:02d}-01"
+            current_run.log_info(f"Retrieve data from: {dateBill_Gte}")
+        
+        # Date input checks - TO
+        if date_year_to is None or date_month_to is None:
+            dateBill_Lte = None
+        else:
+            if not valid_year_check(date_year_to):            
+                return 
+            if not valid_month_check(date_month_to):
+                return
+            date_month_to = int(date_month_to)
+            dateBill_Lte = get_last_day_of_month(f"{date_year_to}-{date_month_to:02d}")          
+            current_run.log_info(f"Retrieve data to: {dateBill_Lte}")
+            
     parameters = {
-        'date_run': f"{date_year}-{date_month:02d}"
+        'dateBill_Gte': dateBill_Gte,
+        'dateBill_Lte': dateBill_Lte,
     }
 
-    current_run.log_info(f"Executing for: {date_year}-{date_month:02d}")
+    current_run.log_info(f"Executing pipeline.")
     
     # Run notebook     
     run_update_with(nb_name=notebook_name, nb_path=notebook_path, out_nb_path=out_notebook_path, parameters=parameters) 
@@ -74,6 +122,30 @@ def run_update_with(nb_name:str, nb_path:str, out_nb_path:str, parameters:dict):
                         parameters=parameters)
 
 
+def get_previous_month_dates():
+    # Get the current date
+    current_date = datetime.now()
+    first_day_of_current_month = current_date.replace(day=1)
+    last_day_of_previous_month = first_day_of_current_month - timedelta(days=1)
+    first_day_of_previous_month = last_day_of_previous_month.replace(day=1)
+
+    # Format the results
+    firstMonthDay = first_day_of_previous_month.strftime('%Y-%m-%d')
+    lastlMonthDay = last_day_of_previous_month.strftime('%Y-%m-%d')
+
+    return firstMonthDay, lastlMonthDay
+
+
+def get_last_day_of_month(date_str):
+    # Parse the input date string
+    date = datetime.strptime(date_str, "%Y-%m")
+    last_day = calendar.monthrange(date.year, date.month)[1]
+    last_day_date = date.replace(day=last_day)
+    
+    # Format the last day
+    return last_day_date.strftime("%Y-%m-%d")
+
+
 # check the year inputs
 def valid_year_check(d_year:str):
     current_date = datetime.now()
@@ -94,7 +166,6 @@ def valid_year_check(d_year:str):
 
 # check the month inputs
 def valid_month_check(d_month:str):
-    current_date = datetime.now()
     
     try:
         d_month = int(d_month)
--- service_extracts/pipeline.py
+++ service_extracts/pipeline.py
@@ -1,30 +1,55 @@
 import os
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+# from dateutil.relativedelta import relativedelta
 import papermill as pm
+import calendar
 
 from openhexa.sdk import current_run, pipeline, parameter, workspace
 
 
 @pipeline(code="service-extracts", name="service_extracts")
 @parameter(
-    "date_year",
-    name="Extract year (e.g: 2024)",
-    help="Execute pipeline for specified year",
+    "date_year_from",
+    name="Extract year from (e.g: 2024)",
+    help="Execute pipeline from specified year",
     type=str,
-    default="2021",
+    default=None,
     required=False,
 )
 @parameter(
-    "date_month",
-    name="Extract month (e.g: 03)",
-    help="Execute pipeline for specified month",
+    "date_month_from",
+    name="Extract month from (e.g: 01)",
+    help="Execute pipeline from specified month",
     type=str,
-    default="12",
+    default=None,
     required=False,
 )
-def service_extracts(date_year:str, date_month:str):
+@parameter(
+    "date_year_to",
+    name="Extract year to (e.g: 2024)",
+    help="Execute pipeline until specified year",
+    type=str,
+    default=None,
+    required=False,
+)
+@parameter(
+    "date_month_to",
+    name="Extract month to (e.g: 03)",
+    help="Execute pipeline untlil specified month",
+    type=str,
+    default=None,
+    required=False,
+)
+@parameter(
+    "run_auto",
+    name="Automatic run (previous month)",
+    help="Execute pipeline for previous month",
+    type=bool,
+    default=True,
+    required=False,
+)
+def service_extracts(run_auto:bool, date_year_from:str, date_month_from:str, date_year_to:str, date_month_to:str):
     """
     In this pipeline we call a notebook that execute the service openIMIS extracts
     
@@ -32,22 +57,44 @@ def service_extracts(date_year:str, date_month:str):
 
     # Setup variables
     notebook_name = "graphQL_services_Niger"  
-    notebook_path = f"{workspace.files_path}/pipelines/Service_extracts/" 
+    notebook_path = f"{workspace.files_path}/pipelines/Service_extracts" 
     out_notebook_path = f"{workspace.files_path}/pipelines/Service_extracts/papermill_outputs"
     
-    # previous_month_date = current_date - relativedelta(months=1)
-    if not valid_year_check(date_year):            
-        return 
-    
-    if not valid_month_check(date_month):
-        return 
-    
-    date_month = int(date_month)
+    if run_auto:
+        dateFrom_Gte, dateFrom_Lte = get_previous_month_dates()
+        current_run.log_info(f"Automatic run from: {dateFrom_Gte} to: {dateFrom_Lte}")    
+
+    else:
+        # Date input checks - FROM
+        if date_year_from is None and date_month_from is None:
+            dateFrom_Gte = None # then we dont filter .. download everything
+        else:
+            if not valid_year_check(date_year_from):            
+                return 
+            if not valid_month_check(date_month_from):
+                return             
+            date_month_from = int(date_month_from)
+            dateFrom_Gte = f"{date_year_from}-{date_month_from:02d}-01"
+            current_run.log_info(f"Retrieve data from: {dateFrom_Gte}")
+        
+        # Date input checks - TO
+        if date_year_to is None or date_month_to is None:
+            dateFrom_Lte = None
+        else:
+            if not valid_year_check(date_year_to):            
+                return 
+            if not valid_month_check(date_month_to):
+                return
+            date_month_to = int(date_month_to)
+            dateFrom_Lte = get_last_day_of_month(f"{date_year_to}-{date_month_to:02d}")        
+            current_run.log_info(f"Retrieve data to: {dateFrom_Lte}")
+            
     parameters = {
-        'date_run': f"{date_year}-{date_month:02d}"
+        'dateFrom_Gte': dateFrom_Gte,
+        'dateFrom_Lte': dateFrom_Lte
     }
 
-    current_run.log_info(f"Executing for: {date_year}-{date_month:02d}")
+    current_run.log_info(f"Executing pipeline.")
     
     # Run notebook     
     run_update_with(nb_name=notebook_name, nb_path=notebook_path, out_nb_path=out_notebook_path, parameters=parameters)
@@ -72,6 +119,31 @@ def run_update_with(nb_name:str, nb_path:str, out_nb_path:str, parameters:dict):
     pm.execute_notebook(input_path = nb_full_path,
                         output_path = out_nb_full_path,
                         parameters=parameters)
+
+
+
+def get_previous_month_dates():
+    # Get the current date
+    current_date = datetime.now()
+    first_day_of_current_month = current_date.replace(day=1)
+    last_day_of_previous_month = first_day_of_current_month - timedelta(days=1)
+    first_day_of_previous_month = last_day_of_previous_month.replace(day=1)
+
+    # Format the results
+    firstMonthDay = first_day_of_previous_month.strftime('%Y-%m-%d')
+    lastlMonthDay = last_day_of_previous_month.strftime('%Y-%m-%d')
+
+    return firstMonthDay, lastlMonthDay
+
+
+def get_last_day_of_month(date_str):
+    # Parse the input date string
+    date = datetime.strptime(date_str, "%Y-%m")
+    last_day = calendar.monthrange(date.year, date.month)[1]
+    last_day_date = date.replace(day=last_day)
+    
+    # Format the last day
+    return last_day_date.strftime("%Y-%m-%d")
 
 
 # check the year inputs
